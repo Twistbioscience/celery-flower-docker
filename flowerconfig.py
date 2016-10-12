b22b88f4452e1841ab073e4f079a29c233c22c59
Arthur Vigil <avigil@twistbioscience.com>
2016-10-12 11:40:23 -0700

    remove basic auth
    using defaults does not provide any additional security, and our flower instance should be operating in a trusted environment

--- flowerconfig.py
+++ flowerconfig.py
@@ -15,6 +15,4 @@ PASSWORD = os.getenv('PASSWORD', 'changeit')
 port = int(os.getenv('FLOWER_PORT', '5555'))
 broker_api = os.getenv('FLOWER_BROKER_API', DEFAULT_BROKER_API)
 max_tasks = int(os.getenv('FLOWER_MAX_TASKS', '3600'))
-basic_auth = [os.getenv('FLOWER_BASIC_AUTH', '%s:%s'
-                        % (USERNAME, PASSWORD))]
 url_prefix = os.getenv('FLOWER_URL_PREFIX', '')
